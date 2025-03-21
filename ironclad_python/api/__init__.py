3dc8f94bde8a9a1d93dba01de60d145469e40d86
Fernando J. Villamarin Diaz <fvil@bendingspoons.com>
2025-03-21 12:33:29 +0100

    chore: update folder structure

---
+++ ironclad_python/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from ironclad_python.api.records_api import RecordsApi
+from ironclad_python.api.webhooks_api import WebhooksApi
+from ironclad_python.api.workflows_api import WorkflowsApi
+
--- openapi_client/api/__init__.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from openapi_client.api.records_api import RecordsApi
-from openapi_client.api.webhooks_api import WebhooksApi
-from openapi_client.api.workflows_api import WorkflowsApi
-
